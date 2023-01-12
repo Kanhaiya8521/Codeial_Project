1064683f888aa013827716c4fabad03de6fb2e40
Kanhaiya kumar <intern4@vama.app>
2023-01-12 22:23:47 +0530

    setup assets folder(css, images, js files)

--- index.js
+++ index.js
@@ -1,8 +1,39 @@
+// const express = require('express');
+// const app = express();
+// const port = 8000;
+// const expressLayouts = require('express-ejs-layouts');
+// app.use(express.static('./assets'));
+// app.use(expressLayouts);
+// // extract style and scripts from sub pages into the layout
+// app.set('layout extractStyles', true);
+// app.set('layout extractScripts', true);
+// // use express router
+// app.use('/', require('./routes'));
+
+// // set up the view engine
+// app.set('view engine', 'ejs');
+// app.set('views', './views');
+
+
+// app.listen(port, function(err){
+//     if (err){
+//         console.log(`Error in running the server: ${err}`);
+//     }
+
+//     console.log(`Server is running on port: ${port}`);
+// });
 const express = require('express');
 const app = express();
 const port = 8000;
 const expressLayouts = require('express-ejs-layouts');
+
+app.use(express.static('./assets'));
+
 app.use(expressLayouts);
+// extract style and scripts from sub pages into the layout
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
+
 
 // use express router
 app.use('/', require('./routes'));
@@ -19,3 +50,4 @@ app.listen(port, function(err){
 
     console.log(`Server is running on port: ${port}`);
 });
+
