81b82833db69868d0eb7d8d3cf52023486d4452c
Kanhaiya kumar <intern4@vama.app>
2023-01-14 00:00:51 +0530

    dummy

--- routes/users.js
+++ routes/users.js
@@ -5,5 +5,7 @@ const usersConrtoller = require('../controllers/users_controller');
 
 router.get('/profile', usersConrtoller.profile);
 
+router.get('/sign-up', usersConrtoller.signUp);
+router.get('/sign-in', usersConrtoller.signIn);
 
-module.exports = router;
+module.exports = router; 
