8c498f4b4c8f8ecfec41f2a763bc5eda31fba387
Kanhaiya kumar <intern4@vama.app>
2023-01-15 19:23:14 +0530

    Added SignIn SignUp and SignOut features

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -8,6 +8,9 @@ module.exports.profile = function(req, res){
 
 // render the sign up page
 module.exports.signUp = function(req, res){
+    if (req.isAuthenticated()){
+       return res.redirect('/users/profile');
+    }
     return res.render('user_sign_up', {
         title: "Codeial | Sign Up"
     })
@@ -15,31 +18,13 @@ module.exports.signUp = function(req, res){
 
 // render the sign in page
 module.exports.signIn = function(req, res){
+    if (req.isAuthenticated()){
+        return res.redirect('/users/profile');
+     }
     return res.render('user_sign_in', {
         title: "Codeial | Sign In"
     })
 }
-
-// get the sign up data
-// module.exports.create = function(req, res){
-//     if(req.body.password != req.body.confirm_password){
-//         return redirect('back');
-//     }
-//     User.findOne({email: req.body.email}, function(err, user){
-//         if(err){console.log('error in finding user in signing up'); return}
-
-//         if(!user){
-//             User.create(req.body, function(err, user){
-//                 if(err){console.log('error in creating user while signing up'); return }
-
-//                 return res.redirect('./users/sign-in');
-//             })
-//         }
-//         else{
-//             return res.redirect('back');
-//         }
-//     })
-// }
 
 module.exports.create = function(req, res){
     if (req.body.password != req.body.confirm_password){
@@ -64,5 +49,15 @@ module.exports.create = function(req, res){
 
 // get the sign In data
 module.exports.createSession = function(req, res){
-    //todo later
+    return res.redirect('/');
+}
+
+
+module.exports.destroySession = function(req, res, next){
+    req.logout(function(err) {
+        if(err){return next(err); }
+        res.redirect('/');
+    });
+
+    // return res.redirect('/');
 }
