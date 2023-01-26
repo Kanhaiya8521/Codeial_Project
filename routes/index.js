66fad7e5246c70f7bc90b0ebb076dd76e90a3e8a
Kanhaiya kumar <intern4@vama.app>
2023-01-26 11:10:53 +0530

    Data base relation: posts and comment

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,8 @@ console.log('router loaded');
 
 router.get('/', homeController.home);
 router.use('/users', require('./users'));
+router.use('/posts', require('./posts'));
+router.use('/comments', require('./comment'));
 
 // for any further routes, access from here
 // router.use('/routerName', require('./routerfile));
