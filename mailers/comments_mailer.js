15d71a592578d73ad0fea1618d55af5b0caa1c3e
Kanhaiya kumar <intern4@vama.app>
2023-04-22 15:17:15 +0530

    successfully added node mail and able to send message through ejs file

--- mailers/comments_mailer.js
+++ mailers/comments_mailer.js
@@ -1,13 +1,14 @@
 const nodemailer = require('./../config/nodemailer');
 
 exports.newComment = (comment) => {
-    console.log('inside newComment mailer', comment);
+    // console.log('inside newComment mailer', comment);
+    let htmlString = nodemailer.renderTemplate({comment: comment}, '../mailers/comments/new_comment.ejs');
 
     nodemailer.transporter.sendMail({
         from: 'kannatokanhaiya@gmail.com',
         to: comment.user.email,
         subject: "New Comment Published!",
-        html: '<h1>Yup, your comment is now published!</h1>'
+        html: htmlString
     }, (err, info) => {
         if(err) {console.log('Error in sending mail', err); return;}
         // console.log('Message sent', info);
