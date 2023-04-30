974bf98b6623b308ce4956ecfac5ecff8476662c
Kanhaiya kumar <kanhaiya15399@gmail.com>
2023-04-30 18:34:03 +0530

    Finally successfully completed the project, added socket io successfully

--- index.js
+++ index.js
@@ -17,6 +17,12 @@ const db = require('./config/mongoose');
  const flash = require('connect-flash');
 
  const customMware = require('./config/middleware');
+
+ // setup the chat server to be used with socket.io
+ const chatServer = require('http').Server(app);
+ const chatSockets = require('./config/chat_sockets').chatSockets(chatServer);
+ chatServer.listen(5000);
+ console.log('Chat server is listening on port 5000');
   
  app.use(sassmiddleware({
     src: './assets/scss',
