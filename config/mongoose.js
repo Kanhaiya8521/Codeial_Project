24c74d1845a4191b0ea0ba888f6332b9c3af5e7d
Kanhaiya kumar <kanhaiya15399@gmail.com>
2023-06-04 23:22:48 +0530

    added mongodb through atlas online

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,14 +1,31 @@
-const mongoose = require('mongoose');
-const env = require('./environment');
-mongoose.connect(`mongodb://localhost/${env.db}`);
+// const mongoose = require('mongoose');
+// const env = require('./environment');
+// mongoose.connect(`mongodb://localhost/${env.db}`);
+// // mongoose.connect('mongodb://localhost/codeial_development');
+
+// const db = mongoose.connection;
+
+// db.on('error', console.error.bind(console, "Error connecting to MongoDB"));
+
+// db.once('open', function(){
+//     console.log('Connected to Database :: MongoDB');
+// });
+
+// module.exports = db;
+
+const mongoose = require("mongoose");
+const env = require("./environment");
+mongoose.connect(
+  `mongodb+srv://kanhaiya15399:Kanhaiya%408521@cluster0.tmqvak8.mongodb.net/?retryWrites=true&w=majority`
+);
 // mongoose.connect('mongodb://localhost/codeial_development');
 
 const db = mongoose.connection;
 
-db.on('error', console.error.bind(console, "Error connecting to MongoDB"));
+db.on("error", console.error.bind(console, "Error connecting to MongoDB"));
 
-db.once('open', function(){
-    console.log('Connected to Database :: MongoDB');
+db.once("open", function () {
+  console.log("Connected to Database :: MongoDB");
 });
 
 module.exports = db;
